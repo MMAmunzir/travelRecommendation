87beda64bc124ff07e80cd1b7ce59129165155dd
MMAmunzir <munzirmuneer1998@gmail.com>
2025-02-21 06:08:20 -0500

    message

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,13 +1,22 @@
- fetch('./travel_recommendation_api.json')
-     .then(res => res.json())
-     .then(data => console.log(data.countries))
-     .catch(error => console.error(error))
-
-
-// const keyword = document.getElementById('keyword').value;
-
 
 function search(){
-const keyword = document.getElementById('keyword').value;
-console.log(keyword)
-}
+const input = document.getElementById('keyword').value.trim();
+
+ fetch('./travel_recommendation_api.json')
+     .then(res => res.json())
+     .then(data => {
+        console.log(data.countries)
+        const condition1 = data.countries.find(item => item.name.toLowerCase() === input.toLowerCase());
+        const condition2 = data.temples.find(item => item.name.toLowerCase() === input.toLowerCase());
+        const condition3 = data.beaches.find(item => item.name.toLowerCase() === input.toLowerCase());
+
+    if(condition1){
+        console.log(condition1)
+    }else if(condition2){
+        console.log(condition2)
+    }else if(condition3){
+        console.log(condition3)
+    }else{
+        console.log("entered is not in data base")
+    }
+     })}
